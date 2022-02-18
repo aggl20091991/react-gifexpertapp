42c130aa3f42b0d6e76816b126dd8d715e263c5f
Leobardo <aggl20091991@hotmail.com>
2022-02-17 18:17:09 -0600

    add s to file

--- src/helper/getGifs.js
+++ src/helper/getGifs.js
@@ -1,18 +1,17 @@
+//peticion asincrona
+export const getGifs = async(category)=>{
+    const url = 'https://api.giphy.com/v1/gifs/search?api_key=oaddHPdy2Ush3FDtsixT6QovxKicH5np&q='
+    +encodeURI(category)+'&limit=10';
+    console.log(category);
+    const response =  await fetch(url);
+    const {data} = await response.json();
+    const gifs = data.map(img =>{
+        return {
+            id:img.id,
+            title:img.title,
+            url:img.images?.downsized_medium.url
+        }
+    })
 
-    //peticion asincrona
-   export const getGifs = async(category)=>{
-        const url = 'https://api.giphy.com/v1/gifs/search?api_key=oaddHPdy2Ush3FDtsixT6QovxKicH5np&q='
-        +encodeURI(category)+'&limit=10';
-        console.log(category);
-        const response =  await fetch(url);
-        const {data} = await response.json();
-        const gifs = data.map(img =>{
-            return {
-                id:img.id,
-                title:img.title,
-                url:img.images?.downsized_medium.url
-            }
-        })
-
-        return gifs
-    }
+    return gifs
+}
